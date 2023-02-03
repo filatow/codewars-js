786b133188089c17384efc5382f1a272df4bac06
evgeniy <not-found@inbox.ru>
2023-02-03 23:44:49 +0300

    Added solution for 'Get the Middle Character'

---
+++ 7 kyu/Get the Middle Character.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56747fd5cb988479af000028
+
+function getMiddle(s) {
+  return s.length % 2
+    ? s[Math.floor(s.length / 2)]
+    : s[s.length / 2 - 1] + s[s.length / 2]
+}
